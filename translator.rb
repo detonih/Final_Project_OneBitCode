305339602920651d9321693fab2386bea7c19826
Henrique Detoni <detonihenrique@gmail.com>
2020-07-14 19:37:14 -0300

    move @url to initialize

--- translator.rb
+++ translator.rb
@@ -2,19 +2,20 @@ require 'rest-client'
 
 class Translator
 
-  # attr_reader :url
+  attr_reader :text, :source_language, :target_language, :url
 
   def initialize(text_picker)
     @text = text_picker.text_picker
     @source_language = text_picker.source_language
     @target_language = text_picker.target_language
-    @url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=#{@source_language}&tl=#{@target_language}&dt=t&q=#{@text}-"
+    @url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=#{source_language}&tl=#{target_language}&dt=t&q=#{text}-"
   end
   
   def translate
 
+
     begin
-      response = RestClient.get @url
+      response = RestClient.get url
 
       regex = /(?<=")(.*?)([-"])/
 
